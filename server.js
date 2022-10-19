f00417df76b0e99d88bad2adb72e55bad9b6def7
Rasika Maduranga <rasikat77@gmail.com>
2022-10-19 16:50:27 +0530

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,14 @@ const express = require('express');
 const app = express();
 const port = 9090;
 
-app.get('/', (req, res) => {
+function sleep(ms) {
+  return new Promise((resolve) => {
+    setTimeout(resolve, ms);
+  });
+}
+
+app.get('/', async (req, res) => {
+  await sleep(4000);
   res.send('Hello World!');
 });
 
